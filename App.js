0e217d4e37264e54ef29bfa94f9b69ef141a38f9
viper9 <durandhippolyte@gmail.com>
2023-11-15 17:50:36 +0100

    Ajout du module Paper et premier essai du bouton2

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { Button } from 'react-native-paper';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Bienvenue sur l'application DreamTalk!</Text>
       <StatusBar style="auto" />
+        <Button icon="camera" title="Press me" >
+            Press me
+        </Button>
+        <Button  title="Presse" icon={{ uri: 'https://avatars0.githubusercontent.com/u/17571969?v=3&s=400' }}>
+            Press me
+        </Button>
+
     </View>
-  );
+
+);
 }
 
 const styles = StyleSheet.create({
