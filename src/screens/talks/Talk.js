23d3386b0aba83f42cd58274659e14dc954a9ba1
Alan <alanohayon@gmail.com>
2023-12-22 14:53:59 +0100

    on voit les msgs que le user envoe

--- src/screens/talks/Talk.js
+++ src/screens/talks/Talk.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList, KeyboardAvoidingView, Platform } from 'react-native';
-import { getDatabase, ref, push, get, onValue } from 'firebase/database'; // Importez les fonctions nécessaires
+import { getDatabase, set, ref, push, get, onValue } from 'firebase/database'; // Importez les fonctions nécessaires
 import { auth } from '../../../firebaseConfig';
 
 const Talk = ({ route, navigation }) => {
@@ -8,24 +8,22 @@ const Talk = ({ route, navigation }) => {
     const { talkId } = route.params;
     const [messages, setMessages] = useState([]);
     const [inputText, setInputText] = useState('');
-    console.log(messages)
+
     useEffect(() => {
         // Récupération initiale des messages
         const fetchMessages = async () => {
             const snapshot = await get(ref(getDatabase(), `talks/${talkId}/messages`));
             if (snapshot.exists()) {
-                const loadedMessages = Object.entries(snapshot.val())
-                    .map(([key, msgData]) => {
-                        return {
-                            id: key,
-                            ...msgData
-                        };
-                    })
-                    // Tri par timestamp décroissant
-                    .sort((a, b) => b.timestamp - a.timestamp);
+                const loadedMessages = [];
+                // On va parcourir tout ce quon à recu
+                snapshot.forEach((childSnapshot) => {
+                    const messageData = childSnapshot.val();
+                    loadedMessages.push(messageData);
+                });
 
+                // Tri par timestamp décroissant
+                loadedMessages.sort((a, b) => b.timestamp - a.timestamp);
                 setMessages(loadedMessages);
-                // console.log(loadedMessages);
             } else {
                 setMessages([]);
             }
@@ -35,15 +33,12 @@ const Talk = ({ route, navigation }) => {
 
         const unsubscribe = onValue(ref(getDatabase(), `talks/${talkId}/messages`), (snapshot) => {
             if (snapshot.exists()) {
-                const updatedMessages = Object.entries(snapshot.val())
-                    .map(([key, msgData]) => {
-                        return {
-                            id: key,
-                            ...msgData
-                        };
-                    })
-                    .sort((a, b) => b.timestamp - a.timestamp);
-
+                const updatedMessages = [];
+                snapshot.forEach((childSnapshot) => {
+                    const messageData = childSnapshot.val();
+                    updatedMessages.push(messageData);
+                });
+                updatedMessages.sort((a, b) => b.timestamp - a.timestamp);
                 setMessages(updatedMessages);
             } else {
                 setMessages([]);
@@ -57,17 +52,14 @@ const Talk = ({ route, navigation }) => {
     const handleSendMessage = async () => {
         if (inputText.trim()) {
             const newMessageRef = push(ref(getDatabase(), `talks/${talkId}/messages`));
-            const newMessage = {
-                senderId: userId,
-                text: inputText,
-                timestamp: Date.now(),
-            };
-
             try {
                 // Enregistrez le nouveau message dans Firebase
-                await set(newMessageRef, newMessage);
-                setMessages(previousMessages => [newMessage, ...previousMessages]);
-                setInputText('');
+                await set(newMessageRef, {
+                    senderId: userId,
+                    text: inputText,
+                    timestamp: Date.now(),
+                });
+
             } catch (error) {
                 console.error('Erreur lors de l\'envoi du message:', error);
                 Alert.alert('Erreur', error.message);
@@ -75,94 +67,88 @@ const Talk = ({ route, navigation }) => {
         }
     };
 
-    const renderMessageItem = ({ item }) => (
-        // affichage des messages
-        <View style={styles.messageItem}>
-            <Text style={styles.messageText}>{item.text}</Text>
-        </View>
-    );
+    const renderMessageItem = ({ item }) => {
+        // Affichez les informations de l'élément dans la console
+        console.log('renderMessageItem item:', item.text);
 
+        return (
+            <View style={styles.messageItem}>
+                <Text style={styles.messageText}>{item.text}</Text>
+                {/* Vous pouvez ajouter plus de logique ici si nécessaire */}
+            </View>
+        );
+    };
+    // messages.forEach(element => {
+    //     console.log('messages:', element.text);
+    // });
     return (
-        <View style={styles.containerBig}>
 
-            <KeyboardAvoidingView
-                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-                style={styles.container}
-                keyboardVerticalOffset={100}
-            >
-                <FlatList
-                    inverted
-                    data={messages}
-                    renderItem={renderMessageItem}
-                    keyExtractor={item => item.id}
-                    style={styles.messagesList}
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={styles.container}
+            keyboardVerticalOffset={100}
+        >
+            <FlatList
+                inverted
+                data={messages}
+                renderItem={renderMessageItem}
+                keyExtractor={(item, index) => index.toString()}
+                style={styles.messagesList}
+            />
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    value={inputText}
+                    onChangeText={setInputText}
+                    placeholder="Écrivez votre message ici..."
                 />
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.input}
-                        value={inputText}
-                        onChangeText={setInputText}
-                        placeholder="Écrivez votre message ici..."
-                    />
-                    <TouchableOpacity style={styles.sendButton} onPress={handleSendMessage}>
-                        <Text style={styles.sendButtonText}>Envoyer</Text>
-                    </TouchableOpacity>
-                </View>
-            </KeyboardAvoidingView>
-
-        </View>
+                <TouchableOpacity style={styles.sendButton} onPress={handleSendMessage}>
+                    <Text style={styles.sendButtonText}>Envoyer</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
 
     );
 };
 
 const styles = StyleSheet.create({
-    containerBig: {
-        flex: 1,
-        backgroundColor: '#fff',
-        position: 'relative',
-        padding: 15,
-    },
     container: {
-        flex: 1,
-        backgroundColor: '#f5f5f5',
+        flex: 1, // Assurez-vous que le conteneur parent prend toute la place
+        backgroundColor: '#fff', // ou toute autre couleur de fond
     },
     messagesList: {
-        flex: 1,
+        flex: 1, // La FlatList doit prendre tout l'espace disponible
     },
     messageItem: {
-        margin: 10,
         padding: 10,
-        backgroundColor: 'blue',
-        borderRadius: 10,
-        maxWidth: '80%',
-        alignSelf: 'flex-end',
+        marginVertical: 5,
+        backgroundColor: 'lightgrey', // Une couleur de fond pour que les messages soient visibles
+        borderRadius: 5,
     },
     messageText: {
-        fontSize: 16,
+        fontSize: 16, // Assurez-vous que la taille de la police est suffisante pour être lue
+        color: 'black', // Assurez-vous que la couleur du texte contraste avec l'arrière-plan
     },
     inputContainer: {
         flexDirection: 'row',
         padding: 10,
         backgroundColor: '#fff',
-        borderTopWidth: 1,
-        borderColor: '#ddd',
     },
     input: {
         flex: 1,
-        padding: 10,
         marginRight: 10,
-        backgroundColor: '#f0f0f0',
-        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: 'grey',
+        borderRadius: 5,
+        padding: 10,
     },
     sendButton: {
-        justifyContent: 'center',
-        alignItems: 'center',
         padding: 10,
-        backgroundColor: '#0066cc',
-        borderRadius: 20,
+        backgroundColor: 'blue',
+        borderRadius: 5,
     },
     sendButtonText: {
-        color: '#fff',
+        color: 'white',
     },
 });
 
